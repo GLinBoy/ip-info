4636e18227c4727327e6ece7a5c8f7bf0cf603bc
GLinBoy <hojjatabedie@gmail.com>
2021-12-03 20:24:47 +0330

    📦 NEW: Add an API to return client IP
    resolved #1

---
+++ src/main/java/com/glinboy/ip/address/web/rest/HomeResource.java
@@ -0,0 +1,40 @@
+package com.glinboy.ip.address.web.rest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/")
+public class HomeResource {
+
+    @Value("${application.header-candidates}")
+    private String[] headerCandidates;
+
+    @GetMapping
+    public ResponseEntity<String> getHome() {
+        if (RequestContextHolder.getRequestAttributes() == null) {
+            return ResponseEntity.ok("0.0.0.0");
+        }
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        log.info("User-Agent: {}", request.getHeader("User-Agent"));
+        for (String header: headerCandidates) {
+            String ipList = request.getHeader(header);
+            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+                String ip = ipList.split(",")[0];
+                return ResponseEntity.ok(ip);
+            }
+        }
+        return ResponseEntity.ok(request.getRemoteAddr());
+    }
+}

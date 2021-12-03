cd491bb3ba9af881f44cadcb5dd9ac0bb3c8e944
GLinBoy <hojjatabedie@gmail.com>
2021-12-03 20:21:58 +0330

    🕯 START: Initiated the project

---
+++ src/main/java/com/glinboy/ip/address/IpAddressApplication.java
@@ -0,0 +1,13 @@
+package com.glinboy.ip.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpAddressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glinboy/ip/address/IpAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.ip.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

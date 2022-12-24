7467fc8af86197fb8def1cb490f68d32a19be1ab
GLinBoy <hojjatabedie@gmail.com>
2022-12-24 13:39:07 +0100

    👌 IMPROVE: Replaced javax.* with jakarta.*

--- src/main/java/com/glinboy/ip/address/web/rest/HomeResource.java
+++ src/main/java/com/glinboy/ip/address/web/rest/HomeResource.java
@@ -1,7 +1,5 @@
 package com.glinboy.ip.address.web.rest;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,24 +16,25 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/")
 public class HomeResource {
 
-    @Value("${application.header-candidates}")
-    private String[] headerCandidates;
+	@Value("${application.header-candidates}")
+	private String[] headerCandidates;
 
-    @GetMapping
-    public ResponseEntity<String> getHome() {
-        if (RequestContextHolder.getRequestAttributes() == null) {
-            return ResponseEntity.ok("0.0.0.0");
-        }
+	@GetMapping
+	public ResponseEntity<String> getHome() {
+		if (RequestContextHolder.getRequestAttributes() == null) {
+			return ResponseEntity.ok("0.0.0.0");
+		}
 
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        log.info("User-Agent: {}", request.getHeader("User-Agent"));
-        for (String header: headerCandidates) {
-            String ipList = request.getHeader(header);
-            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
-                String ip = ipList.split(",")[0];
-                return ResponseEntity.ok(ip);
-            }
-        }
-        return ResponseEntity.ok(request.getRemoteAddr());
-    }
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+				.getRequest();
+		log.info("User-Agent: {}", request.getHeader("User-Agent"));
+		for (String header : headerCandidates) {
+			String ipList = request.getHeader(header);
+			if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+				String ip = ipList.split(",")[0];
+				return ResponseEntity.ok(ip);
+			}
+		}
+		return ResponseEntity.ok(request.getRemoteAddr());
+	}
 }

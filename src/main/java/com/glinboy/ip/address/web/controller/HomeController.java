5fa6ed2dcf406bdfa61860dca071fc2c9b4269a0
GLinBoy <hojjatabedie@gmail.com>
2023-07-16 13:08:57 +0200

    📦 NEW: Added an endpoint for JSON response

--- src/main/java/com/glinboy/ip/address/web/controller/HomeController.java
+++ src/main/java/com/glinboy/ip/address/web/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.glinboy.ip.address.web.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.glinboy.ip.address.model.dto.IpInfoResponseDTO;
 import com.glinboy.ip.address.model.enums.ResponseType;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -58,5 +60,25 @@ public class HomeController {
 		userIpOptional.ifPresentOrElse(i -> model.addAttribute(USER_IP_NAME, i),
 				() -> model.addAttribute(USER_IP_NAME, request.getRemoteAddr()));
 		return "index";
+	}
+
+	@GetMapping("/index.json")
+	public ResponseEntity<IpInfoResponseDTO> getHomeJson(HttpServletRequest request) {
+		Optional<String> userIpOptional = Optional.empty();
+		IpInfoResponseDTO ipInfo = IpInfoResponseDTO.builder().build();
+
+		if (RequestContextHolder.getRequestAttributes() == null) {
+			userIpOptional = Optional.of("0.0.0.0");
+		}
+
+		for (String header : headerCandidates) {
+			String ipList = request.getHeader(header);
+			if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+				userIpOptional = Optional.of(ipList.split(",")[0]);
+			}
+		}
+
+		userIpOptional.ifPresentOrElse(ipInfo::setIp, () -> ipInfo.setIp(request.getRemoteAddr()));
+		return ResponseEntity.ok(ipInfo);
 	}
 }

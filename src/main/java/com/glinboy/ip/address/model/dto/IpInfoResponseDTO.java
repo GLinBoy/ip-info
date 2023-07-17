b13295934484a9d46cd86c235f7483dcae8927a7
GLinBoy <hojjatabedie@gmail.com>
2023-07-17 19:52:13 +0200

    📦 NEW: Updated JSON response and add headers

--- src/main/java/com/glinboy/ip/address/model/dto/IpInfoResponseDTO.java
+++ src/main/java/com/glinboy/ip/address/model/dto/IpInfoResponseDTO.java
@@ -1,10 +1,16 @@
 package com.glinboy.ip.address.model.dto;
 
+import org.springframework.util.MultiValueMap;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class IpInfoResponseDTO {
 	private String ip;
+	private MultiValueMap<String, String> headears;
 }
